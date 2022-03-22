d4c82b12042851844aed9007528174e5414abc1a
charan-123 <69838278+charan-123@users.noreply.github.com>
2022-03-22 12:22:16 +0530

    a

---
+++ index.js
@@ -0,0 +1,27 @@
+/*function varsha()
+{
+  var randomNumber1 = Math.floor((Math.random()*6)+1);
+  var k= "images/dice"+randomNumber1+".png"
+  return k;
+}*/
+// var l =varsha();
+// document.querySelector("img")[0]=setAttribute("src",l);
+var randomNumber1 = Math.floor((Math.random()*6)+1);
+var source = "images/dice"+randomNumber1+".png";
+document.querySelectorAll("img")[0].setAttribute("src",source);
+var randomNumber2 = Math.floor((Math.random()*6)+1);
+var source2 = "images/dice"+randomNumber2+".png";
+document.querySelectorAll("img")[1].setAttribute("src",source2);
+
+if (randomNumber1==randomNumber2)
+{
+  document.getElementById("winner").innerHTML="DRAW";
+}
+else if (randomNumber1 > randomNumber2)
+{
+    document.getElementById("winner").innerHTML="Gammu IS WINNER";
+}
+else if (randomNumber1 < randomNumber2)
+{
+  document.getElementById("winner").innerHTML="Charan IS WINNER";
+}
